60c9b1e34a1b740ec184f3e46b6baedfa34b803e
weisk <franml87@gmail.com>
2026-05-22 22:00:58 +0200

    chore: fix gh-pages nextjs config

--- apps/poc-0/next.config.ts
+++ apps/poc-0/next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
+const repo = "project-elixir";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
